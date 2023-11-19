682f1258193a09790101cf4dfba11b54ad9e6159
patricia.yogi@gmail.com <patricia.yogi@gmail.com>
2023-11-18 18:39:46 -0800

    adding image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img
+          className="object-cover object-center rounded"
+          alt="Me"
+          src="./project-1.gif"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
